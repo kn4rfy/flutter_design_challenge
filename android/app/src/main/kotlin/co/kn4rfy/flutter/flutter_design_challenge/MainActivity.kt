0aa805775a892414cd69b61224c0f744235a8c33
Francis Knarfy Elopre <kn4rfy@gmail.com>
2019-05-23 16:14:42 +0800

    flutter init

---
+++ android/app/src/main/kotlin/co/kn4rfy/flutter/flutter_design_challenge/MainActivity.kt
@@ -0,0 +1,13 @@
+package co.kn4rfy.flutter.flutter_design_challenge
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
